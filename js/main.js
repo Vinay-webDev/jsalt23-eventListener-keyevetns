bc8ea2f22fc6102a6561adb843e2cab7f328b7e8
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-17 23:51:02 +0530

    keydown and keyup event to backgroundColor and textContent

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
 // eventListener
 
-
+/*
 document.addEventListener("keydown", event => {
     console.log(`key down = ${event.key}`);
 })
 
 document.addEventListener("keyup", event => {
     console.log(`key up = ${event.key}`);
-}) 
+})*/
 
+const myBox = document.getElementById("myBox");
 
+document.addEventListener("keydown", event => {
+    myBox.textContent = "😲";
+    myBox.style.backgroundColor = "tomato";
+})
+
+document.addEventListener("keyup", event => {
+    myBox.textContent = "😀";
+    myBox.style.backgroundColor = "cyan";
+})
 
 
 
