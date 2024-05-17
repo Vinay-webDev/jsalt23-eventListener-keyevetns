27d9782d3df2f7f8206f02f82663703cfa558f96
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-17 23:39:44 +0530

    keyup and keydown events😀😀

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 // eventListener
 
 
+document.addEventListener("keydown", event => {
+    console.log(`key down = ${event.key}`);
+})
 
-
-
+document.addEventListener("keyup", event => {
+    console.log(`key up = ${event.key}`);
+}) 
 
 
 
