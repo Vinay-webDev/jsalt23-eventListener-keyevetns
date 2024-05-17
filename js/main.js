8bbe7d000ed5e89bd2e971ec88f116ad712b61ed
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-17 23:59:13 +0530

    movement😀😲

--- js/main.js
+++ js/main.js
@@ -11,6 +11,10 @@ document.addEventListener("keyup", event => {
 
 const myBox = document.getElementById("myBox");
 
+const moveAmount = 10;
+let y = 0;
+let x = 0;
+
 document.addEventListener("keydown", event => {
     myBox.textContent = "😲";
     myBox.style.backgroundColor = "tomato";
@@ -21,6 +25,27 @@ document.addEventListener("keyup", event => {
     myBox.style.backgroundColor = "cyan";
 })
 
+document.addEventListener("keydown", event => {
+    
+    if (event.key.startsWith("Arrow")) {
+        switch (event.key) {
+            case "ArrowUp":
+                y -= moveAmount;
+                break;
+            case "ArrowDown": 
+                y += moveAmount;
+                break;
+            case "ArrowLeft": 
+                x -= moveAmount;
+                break;
+            case "ArrowRight": 
+                x += moveAmount;
+                break;
+        }
+        myBox.style.top = `${y}px`;
+        myBox.style.left = `${x}px`;
+    }
+})
 
 
 
